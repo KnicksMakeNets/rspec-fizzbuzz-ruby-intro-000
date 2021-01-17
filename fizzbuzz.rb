c2625d87244a128b6e5438f22364cf9a09eb4799
Daniel Costelloe <danny.costelloe@gmail.com>
2021-01-17 14:20:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,13 @@
 def fizzBuzz(int)
   if int % 3 == 0
     "Fizz"
+    
   elsif int % 5 == 0
     "Buzz"
+    
   elsif int % 3 == 0 && number % 5 == 0
     "FizzBuzz"
+    
   else
     int
   end

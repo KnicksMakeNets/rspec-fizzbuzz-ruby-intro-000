d5aee599ba9ee3b68cb5664e15cfa25ae5ae4126
Daniel Costelloe <danny.costelloe@gmail.com>
2021-01-17 14:10:19 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,11 +4,14 @@ def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
 
-  elsif int % 5 == 0
-      "Buzz"
+  elsif int % 5 == 0 # if the number int is divisible by 5
+      "Buzz" # Go Buzz
 
-  elsif int % 3 && 5  == 0
-      "fizzbuzz"
+  elsif int %
+        
+
+  else int % 3 && 5  == 0 # if the number int is not diviible by 3 or 5
+      "FizzBuzz"
 
   end
 end
